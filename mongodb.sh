68c30d0585f4206ba37c8b67439e3e00eceb7abc
udayayeswari <udayeswari.savana@gmail.co>
2023-02-20 17:14:29 -0500

    adding condition

--- mongodb.sh
+++ mongodb.sh
@@ -1,44 +1,122 @@
 adduser spec
 
 curl -sL https://rpm.nodesource.com/setup_16.x | bash -
-echo status = $?
+if [$? -eq  0]
+ then
+  echo status = SUCCESS
+ else
+   echo status = FAILURE
+  fi
 
 yum install nodejs -y
-echo status = $?
+if [$? -eq  0]
+ then
+  echo status = SUCCESS
+ else
+   echo status = FAILURE
+  fi
+
 
 yum install git -y
-echo status = $?
+if [$? -eq  0]
+ then
+  echo status = SUCCESS
+ else
+   echo status = FAILURE
+  fi
 
 cd /home/spec/
-echo status = $?
+if [$? -eq  0]
+ then
+  echo status = SUCCESS
+ else
+   echo status = FAILURE
+  fi
+
 git clone https://github.com/udayayeswar/Mongodb.git
-echo status = $?
+if [$? -eq  0]
+ then
+  echo status = SUCCESS
+ else
+   echo status = FAILURE
+  fi
 cd app/
 cat package.sh | bash
-echo status = $?
+if [$? -eq  0]
+ then
+  echo status = SUCCESS
+ else
+   echo status = FAILURE
+  fi
+
 
 db_user=udaya
 db_pass=udaya123
 
 echo Environment="MONGO_ENDPOINT=mongodb+srv://$db_user:$db_pass@cluster0.nggvzyx.mongodb.net/login-app-db?retryWrites=true&w=majority" >> files/spec.service
-echo status = $?
+if [$? -eq  0]
+ then
+  echo status = SUCCESS
+ else
+   echo status = FAILURE
+  fi
 cp files/spec.service /etc/systemd/system/
-echo status = $?
+if [$? -eq  0]
+ then
+  echo status = SUCCESS
+ else
+   echo status = FAILURE
+  fi
 systemctl start spec
-echo status = $?
+if [$? -eq  0]
+ then
+  echo status = SUCCESS
+ else
+   echo status = FAILURE
+  fi
 
 sudo yum install epel-release -y
-echo status = $?
+if [$? -eq  0]
+ then
+  echo status = SUCCESS
+ else
+   echo status = FAILURE
+  fi
 sudo yum install nginx -y
-echo status = $?
+if [$? -eq  0]
+ then
+  echo status = SUCCESS
+ else
+   echo status = FAILURE
+  fi
 
 yes | cp -rf files/nginx.conf /etc/nginx/nginx.conf
-echo status = $?
+if [$? -eq  0]
+ then
+  echo status = SUCCESS
+ else
+   echo status = FAILURE
+  fi
 yes | cp -rf files/nodejs.conf /etc/nginx/conf.d/nodejs.conf
-echo status = $?
+if [$? -eq  0]
+ then
+  echo status = SUCCESS
+ else
+   echo status = FAILURE
+  fi
 setenforce 0
-echo status = $?
+if [$? -eq  0]
+ then
+  echo status = SUCCESS
+ else
+   echo status = FAILURE
+  fi
 systemctl restart nginx
-echo status = $?
+if [$? -eq  0]
+ then
+  echo status = SUCCESS
+ else
+   echo status = FAILURE
+  fi
 # node .js > node.logs 2>&1 &
 # ps -ef | grep "index.js" > run.log
