8ddc74a42a7e0876f81fbd87b2173deff270cfae
udayayeswari <udayeswari.savana@gmail.co>
2023-02-20 17:48:13 -0500

    deleting the condition

--- mongodb.sh
+++ mongodb.sh
@@ -1,14 +1,11 @@
 adduser spec
 
 curl -sL https://rpm.nodesource.com/setup_16.x | bash -
-
 yum install nodejs -y
+
 yum install git -y
-
 cd /home/spec/
-
-git clone https://github.com/udayayeswar/app.git
-
+git clone https://github.com/ChaitanyaChandra/app.git
 cd app/
 cat package.sh | bash
 
@@ -17,7 +14,6 @@ db_pass=udaya123
 
 echo Environment="MONGO_ENDPOINT=mongodb+srv://$db_user:$db_pass@cluster0.nggvzyx.mongodb.net/login-app-db?retryWrites=true&w=majority" >> files/spec.service
 cp files/spec.service /etc/systemd/system/
-
 systemctl start spec
 
 sudo yum install epel-release -y
@@ -25,9 +21,7 @@ sudo yum install nginx -y
 
 yes | cp -rf files/nginx.conf /etc/nginx/nginx.conf
 yes | cp -rf files/nodejs.conf /etc/nginx/conf.d/nodejs.conf
-
 setenforce 0
-
 systemctl restart nginx
 # node .js > node.logs 2>&1 &
 # ps -ef | grep "index.js" > run.log
